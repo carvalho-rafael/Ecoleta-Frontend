169633cd1cf9c404fa4d6607250235447fa72655
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2020-10-11 21:08:51 -0300

    create index page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,26 +4,28 @@ import { FiLogIn } from 'react-icons/fi'
 import logo from '../../images/trash.svg'
 import { Link } from 'react-router-dom'
 
-const Home = () => (
-    <div id="page-home">
-        <div className="content">
-            <header>
-                <img src={logo} alt="Ecoleta" width="30px" />
-                <h2 className="logo-text">Ecoleta</h2>
-            </header>
-            <main>
-                <h1>O mundo agradece a sua ajuda.</h1>
-                <p>Encontre um ponto de coleta perto de você e faça a diferença!</p>
-                <Link to="/create-point">
-                    <span>
-                        <FiLogIn />
-                    </span>
-                    <strong>Cadastre-se</strong>
-                </Link>
-            </main>
-        </div>
+const Home = () => {
+    return (
+        <div id="page-home">
+            <div className="content">
+                <header>
+                    <img src={logo} alt="Ecoleta" width="30px" />
+                    <h2 className="logo-text">Ecoleta</h2>
+                </header>
+                <main>
+                    <h1>O mundo agradece a sua ajuda.</h1>
+                    <p>Encontre um ponto de coleta perto de você e faça a diferença!</p>
+                    <Link to="/create-point">
+                        <span>
+                            <FiLogIn />
+                        </span>
+                        <strong>Cadastre-se</strong>
+                    </Link>
+                </main>
+            </div>
 
-    </div>
-)
+        </div>
+    )
+}
 
 export default Home;
